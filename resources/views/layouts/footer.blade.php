c5066b59965ed08fcd44ea70cbbe48d834a458bd
Lean998 <leandroaguero83@gmail.com>
2025-08-25 15:23:38 -0300

    Create footer.blade.php

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="py-1">
+    <ul class="nav justify-content-center">
+        <li class="nav-item"><a href="#" class="px-2 fs-h1 fw-bold color-sand nav-link text-reset text-decoration-none">Mi Panaderia</a></li>
+    </ul>
+    <p class="text-center fs-h3">© COPYRIGHT 2025. TODOS LOS DERECHOS RESERVADOS </p>
+</footer>
