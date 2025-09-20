1c590592b61e543a9de1f488671537b369bc825c
Lean998 <leandroaguero83@gmail.com>
2025-09-20 19:48:00 -0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,33 @@
         <meta charset="UTF-8">
         <title>@yield('title', 'El funito')</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @stack('styles')
     </head>
 <body class="background">
     {{-- Incluir header --}}
     @include('layouts.header')
-
+        @if (session('success'))
+            <div class="container mt-3 align-items-center">
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <p class="fw-semibold">{{ session('success') }} </p>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            </div>
+        @elseif (session('warning'))
+            <div class="container mt-3 align-items-center">
+                <div class="alert light alert-dismissible show border-chocolate" role="alert" style="background-color: #F9D976;">
+                    <p class="fw-semibold">{{ session('warning') }} </p>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            </div>
+        @elseif (session('error'))
+            <div class="container mt-3 align-items-center">
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <p class="fw-semibold">{{ session('error') }} </p>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            </div>
+        @endif
     <main>
         @yield('content')
     </main>
