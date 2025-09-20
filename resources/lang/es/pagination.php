6622cf6d7bcabd919437adeaf9690626015213cc
Lean998 <leandroaguero83@gmail.com>
2025-09-20 19:48:21 -0300

    Create pagination.php

---
+++ resources/lang/es/pagination.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'previous' => '&laquo; Anterior',
+    'next'     => 'Siguiente &raquo;',
+    'showing'  => 'Mostrando',
+    'to'       => 'a',
+    'results' => 'resultados',
+];
