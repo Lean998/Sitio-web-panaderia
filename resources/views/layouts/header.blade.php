53d29bc5eae62e1258fac799fd828a4abbbba9a4
Lean998 <leandroaguero83@gmail.com>
2025-09-20 19:47:57 -0300

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,20 +1,17 @@
 <header class="d-flex flex-wrap justify-content-center">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
         <span class="fs-h1 fw-bold mx-3">El Funito</span>
-        <svg class="bi me-2" width="40" height="32">
-            <use xlink:href="#bootstrap"></use>
-        </svg>
     </a>
 
     <ul class="nav">
-        <li class="nav-item"><a href="#" class="border-sand  py-3 px-4 fw-semibold fs-h3 nav-link text-reset text-decoration-none active" aria-current="page">Productos</a></li>
+        <li class="nav-item"><a href="/productos" class="border-sand  py-3 px-4 fw-semibold fs-h3 nav-link text-reset text-decoration-none active" aria-current="page">Productos</a></li>
         <li class="nav-item"><a href="#" class="border-sand  py-3 px-4 fw-semibold fs-h3 nav-link text-reset text-decoration-none">Sucursal</a></li>
         <li class="nav-item"><a href="#" class="border-sand  py-3 px-4 fw-semibold fs-h3 nav-link text-reset text-decoration-none">Contacto</a></li>
+        <li class="nav-item"><a href="{{ route('favoritos') }}" class="border-sand  py-3 px-4 fw-semibold fs-h3 nav-link text-reset text-decoration-none">Favoritos</a></li>
         <li class="nav-item" id="cart-link">
-            <a href="#" class="nav-link py-3 px-4">
-                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-                </svg>
+            <a href="{{ route('carrito') }}" class="nav-link py-3 px-4">
+                <x-cart 
+                />
             </a>
         </li>
     </ul>
