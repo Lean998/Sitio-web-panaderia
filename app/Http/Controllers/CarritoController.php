69d58c9487c4a51fd9aa887bd61bc70151e19a26
Lean998 <leandroaguero83@gmail.com>
2025-09-25 11:04:59 -0300

    Update CarritoController.php

--- app/Http/Controllers/CarritoController.php
+++ app/Http/Controllers/CarritoController.php
@@ -15,36 +15,50 @@ class CarritoController extends Controller
 
     public function agregarAlCarrito(Request $request, $productoId = null, $cantidad = 1){
         $carrito = session()->get('carrito', []);
-        if (isset($carrito[$productoId])) {
-            $producto = Producto::find($productoId);
-            if($producto->cantidad - $carrito[$productoId]['cantidad'] + $cantidad <= 0){
-                return back()->with('error', 'No hay stock disponible para agregar otra unidad de '.$producto->nombre);
-            }
-        $carrito[$productoId]['cantidad'] += $cantidad;
-        session()->put('carrito', $carrito);
-        Carrito::actualizarCarrito(session()->getId(), session()->get('carrito', []));
-        if(!$this->actualizarCarrito()){
-            return back()->with('error', 'Ocurrio un error inesperado al agregar el producto al carrito de compras.');
-        }
-        return back()->with('success', 'Agregaste una unidad de '.$producto->nombre.' al carrito.');
-        } else {
         $producto = Producto::find($productoId);
-        if($producto->cantidad - $cantidad <= 0){
-            return back()->with('error', 'No hay stock disponible para agregar otra unidad de '.$producto->nombre);
-        }
-        $carrito[$productoId] = [
-            "id" => $producto->id,
-            "nombre" => $producto->nombre,
-            "cantidad" => $cantidad,
-            "precio" => $producto->precio,
-            "imagen" => $producto->imagen
-        ];
+        if (isset($carrito[$productoId])) {
+            
+
+            $cantidadEnCarrito = $carrito[$productoId]['cantidad'] ?? 0;
+
+            $total = round($cantidadEnCarrito + $cantidad, 2);
+            $stock = round($producto->cantidad, 2);
+
+            if ($total > $stock) {
+                return back()->with('error', 'No hay stock disponible para agregar '.$cantidad.' de '.$producto->nombre);
+            }
+
+            $carrito[$productoId]['cantidad'] = $total;
+            session()->put('carrito', $carrito);
+            
+            Carrito::actualizarCarrito(session()->getId(), session()->get('carrito', []));
+            
+            if(!$this->actualizarCarrito()){
+                return back()->with('error', 'Ocurrio un error inesperado al agregar el producto al carrito de compras.');
+            }
+
+            return back()->with('success', 'Agregaste una unidad de '.$producto->nombre.' al carrito.');
+        } else {
+            if($producto->cantidad - $cantidad < 0){
+                return back()->with('error', 'No hay stock disponible para agregar '.$producto->nombre);
+            }
+            $carrito[$productoId] = [
+                "id" => $producto->id,
+                "nombre" => $producto->nombre,
+                "cantidad" => $cantidad,
+                'unidad_venta' => $producto->unidad_venta,
+                "precio" => $producto->precio,
+                "imagen" => $producto->imagen
+            ];
     }
+
     session()->put('carrito', $carrito);
     Carrito::actualizarCarrito(session()->getId(), session()->get('carrito', []));
+    
     if(!$this->actualizarCarrito()){
         return back()->with('error', 'Ocurrio un error inesperado al agregar el producto al carrito de compras.');
     }
+    
     return back()->with('success', 'Agregaste '. $producto->nombre .' al carrito de compras.');
 }
 
@@ -62,11 +76,16 @@ public function eliminarProducto($productoId){
     return back()->with('warning', 'Eliminaste '.$nombre.' del carrito de compras.');
 }
 
-public function eliminarUnidad($productoId){
+public function eliminarUnidad(Request $request, $productoId, $cantidad = null){
     $carrito = session()->get('carrito', []);
     $nombre = '';
     if (isset($carrito[$productoId])) {
-        $carrito[$productoId]['cantidad']--;
+        if($request->filled('cantidad')){
+            $cantidad = abs(floatval($request->input('cantidad')));  
+        } else{
+            $cantidad = 1; // Valor por defecto si no se proporciona
+        }
+        $carrito[$productoId]['cantidad']-= $cantidad;
         if ($carrito[$productoId]['cantidad'] <= 0) {
             $nombre = $carrito[$productoId]['nombre'];
             unset($carrito[$productoId]);
@@ -85,20 +104,36 @@ public function eliminarUnidad($productoId){
     return redirect()->to('carrito');
 }
 
-public function agregarUnidad($productoId){
+public function agregarUnidad(Request $request, $productoId, $cantidad = null){
     $carrito = session()->get('carrito', []);
 
     if (isset($carrito[$productoId])) {
         $producto = Producto::find($productoId);
-        if($producto->cantidad - $carrito[$productoId]['cantidad'] <= 0){
-            return back()->with('error', 'No hay stock disponible para agregar otra unidad de '.$producto->nombre);
+
+        if ($request->filled('cantidad')) {
+            $cantidad = abs(floatval($request->input('cantidad')));
+        } else {
+            $cantidad = 1; // Valor por defecto
         }
-        $carrito[$productoId]['cantidad']++;
+
+        $cantidadEnCarrito = $carrito[$productoId]['cantidad'] ?? 0;
+
+        $total = round($cantidadEnCarrito + $cantidad, 2);
+        $stock = round($producto->cantidad, 2);
+
+        if ($total > $stock) {
+            return back()->with('error', 'No hay stock disponible para agregar '.$cantidad.' de '.$producto->nombre);
+        }
+
+        $carrito[$productoId]['cantidad'] = $total;
+        
         session()->put('carrito', $carrito);
     }
+
     if(!Carrito::actualizarCarrito(session()->getId(), session()->get('carrito', []))){
         return back()->with('error', 'Ocurrio un error inesperado al agregar '.$producto->nombre.' al carrito de compras.');
     }
+
     if(!$this->actualizarCarrito()){
         return back()->with('error', 'Ocurrio un error inesperado al agregar '.$producto->nombre.' al carrito de compras.');
     }
