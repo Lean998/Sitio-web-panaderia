44280e6488b2a25f805aa2b421ed17e04fac1695
Lean998 <leandroaguero83@gmail.com>
2025-09-28 14:16:24 -0300

    Create validation.php

---
+++ resources/lang/es/validation.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'required' => 'El campo :attribute es obligatorio.',
+    'email' => 'El campo :attribute debe ser una dirección de correo válida.',
+    'min' => [
+        'string' => 'El campo :attribute debe contener al menos :min caracteres.',
+    ],
+    'max' => [
+        'string' => 'El campo :attribute no debe ser mayor que :max caracteres.',
+    ],
+
+    'attributes' => [
+        'nombre' => 'nombre',
+        'email' => 'correo electrónico',
+        'mensaje' => 'mensaje',
+    ],
+];
