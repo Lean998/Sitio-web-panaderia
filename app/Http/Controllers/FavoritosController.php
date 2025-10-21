33d4cccd87669ab9b72fb45df3a4c6658b02e6c8
Lean998 <leandroaguero83@gmail.com>
2025-10-21 20:59:52 -0300

    Update FavoritosController.php

--- app/Http/Controllers/FavoritosController.php
+++ app/Http/Controllers/FavoritosController.php
@@ -36,9 +36,13 @@ class FavoritosController extends Controller
 
     public function eliminarProducto($productoId)
     {
+        $producto = Producto::findOrFail($productoId);
+        if(!$producto) {
+            return back()->with('error', "Producto no encontrado.");
+        }
         try {
             $this->favoritosService->eliminarProducto($productoId);
-            return back()->with('warning', "Eliminaste el producto de la lista de favoritos.");
+            return back()->with('warning', "Eliminaste {$producto->nombre} de la lista de favoritos.");
         } catch (Exception $e) {
             return back()->with('error', $e->getMessage());
         }
@@ -49,6 +53,23 @@ class FavoritosController extends Controller
         try {
             $this->favoritosService->vaciarFavoritos();
             return back()->with('success', "Lista de favoritos vaciada con éxito.");
+        } catch (Exception $e) {
+            return back()->with('error', $e->getMessage());
+        }
+    }
+
+    public function toggle($productoId){
+        $producto = Producto::findOrFail($productoId);
+
+        try {
+            $agregado = $this->favoritosService->toggle($producto);
+            
+            $mensaje = $agregado 
+                ? "Agregaste {$producto->nombre} a favoritos"
+                : "Eliminaste {$producto->nombre} de favoritos";
+            
+            return back()->with($agregado ? 'success' : 'warning', $mensaje);
+            
         } catch (Exception $e) {
             return back()->with('error', $e->getMessage());
         }
