4c4eb56f5944efde83f8d7c2fed36fa6c2c1d9e3
Lean998 <leandroaguero83@gmail.com>
2025-10-21 20:43:27 -0300

    Migracion pedidos items

---
+++ database/migrations/2025_10_14_205804_create_pedido_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pedido_items', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('pedido_id')->constrained('pedidos')->onDelete('cascade');
+        $table->foreignId('producto_id')->constrained('productos')->onDelete('restrict');
+        $table->string('nombre_producto', 100);
+        $table->string('unidad_venta', 20);
+        
+        $table->decimal('cantidad', 8, 2);
+        $table->decimal('precio_unitario', 10, 2);
+        $table->decimal('subtotal', 10, 2);
+        $table->timestamps();
+        
+        $table->index(['producto_id', 'created_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pedido_items');
+    }
+};
