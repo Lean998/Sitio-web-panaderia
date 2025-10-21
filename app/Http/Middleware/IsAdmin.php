284584daee39416f433942278eb85d775759c823
Lean998 <leandroaguero83@gmail.com>
2025-10-21 20:58:56 -0300

    Create IsAdmin.php
    Middleware para verificar que el usuario que ingrese a las rutas de admin efectivamente sea un administrador valido.

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class IsAdmin
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!Auth::check()) {
+            return redirect()->route('admin.login')
+                ->with('error', 'Debes iniciar sesión como administrador');
+        }
+
+        
+        if (Auth::user()->role !== 'admin') {
+            abort(403, 'No tienes permisos para acceder a esta área');
+        }
+
+        if (!session()->get('admin_in', false)) {
+            session()->put('admin_in', true); // Solo si el usuario es admin
+        }
+
+        return $next($request);
+    }
+}
+
+
