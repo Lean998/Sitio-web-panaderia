1ca8d23fa0da00712b5a7fe779d9403d9a032929
Lean998 <leandroaguero83@gmail.com>
2025-10-21 20:50:39 -0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>@yield('title', 'El funito')</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @stack('styles')
     </head>
@@ -37,5 +38,6 @@
     
     {{-- Incluir footer --}}
     @include('layouts.footer')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
