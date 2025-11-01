6a9a819c3ea5c0d4a1e73053763dea4a83876a37
Lean998 <leandroaguero83@gmail.com>
2025-11-01 20:50:51 -0300

    Update headerAdmin.blade.php

--- resources/views/layouts/admin/headerAdmin.blade.php
+++ resources/views/layouts/admin/headerAdmin.blade.php
@@ -11,21 +11,28 @@
 
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
+                    @if (session()->get('admin_role') === 'admin')
                     <li class="nav-item">
                         <a href="{{ route('admin.productos') }}" class="border-sand py-3 px-4 fw-semibold fs-h3 nav-link text-reset text-decoration-none active" aria-current="page">Productos</a>
                     </li>
+                    
                     <li class="nav-item">
                         <a href="{{ route('admin.dashboard') }}" class="border-sand py-3 px-4 fw-semibold fs-h3 nav-link text-reset text-decoration-none active" aria-current="page">Dashboard</a>
                     </li>
+                    @endif
+
                     <li class="nav-item">
                         <a href="{{ route('admin.pedidos.index') }}" class="border-sand py-3 px-4 fw-semibold fs-h3 nav-link text-reset text-decoration-none active" aria-current="page"> Pedidos </a>
                     </li>
+
                     <li class="nav-item">
                         <a href="{{ route('admin.stock.index') }}" class="border-sand py-3 px-4 fw-semibold fs-h3 nav-link text-reset text-decoration-none active" aria-current="page">Gestión de Stock</a>
                     </li>
+                    @if (session()->get('admin_role') === 'admin')
                     <li class="nav-item">
                         <a href="{{ route('admin.pedidos.estadisticas') }}" class="border-sand py-3 px-4 fw-semibold fs-h3 nav-link text-reset text-decoration-none active" aria-current="page"> Estadísticas </a>
                     </li>
+                    @endif
                     <li>
                         <a href="#" onclick="event.preventDefault(); document.getElementById('formEliminar').submit();" class="py-3 px-4 fw-semibold fs-h3 nav-link text-reset text-decoration-none active" aria-current="page"> Cerrar Sesion </a>
                         <form id="formEliminar" action="{{ route('admin.logout') }}" method="POST" style="display:none;">
