e1c5988de9123932ae25f771f91623ad0367dfda
Lean998 <leandroaguero83@gmail.com>
2025-11-01 20:41:07 -0300

    Update AdminController.php
    Logica para crear empleados

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Services\ProductoService;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use App\Models\User;
 class AdminController extends Controller
 {
     protected ProductoService $productService;
@@ -29,13 +33,44 @@ class AdminController extends Controller
         ]);
         
         $tipos = [
-            'Panaderia' => ['Medialunas', 'Pan integral', 'Baguette'],
+            'Panaderia' => ['Medialunas', 'Pan integral', 'Baguette', 'Tortitas', 'Pan', 'Pan casero'],
             'Pasteleria' => ['Tortas', 'Tartas', 'Postres'],
             'Salados'   => ['Empanadas', 'Pizzetas', 'Facturas saladas'],
-            'Todos'     => ['Medialunas', 'Pan integral', 'Baguette', 'Tortas', 'Tartas', 'Postres', 'Empanadas', 'Pizzetas', 'Facturas saladas'],
+            'Todos'     => ['Medialunas', 'Pan integral', 'Baguette', 'Tortas', 'Tartas', 'Postres', 'Empanadas', 'Pizzetas', 'Facturas saladas', 'Pan', 'Pan casero', 'Tortitas'],
         ];
 
         return view('admin.productos.index', ['productos' => $productos, 'categoriaKey' => $categoria ? ucfirst($categoria) : 'Todos', 'tipos' => $tipos]);
+    }
+
+    public function create()
+    {
+        return view('admin.usuarios.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+        'name' => 'required|string|min:3|max:100',
+        'email' => 'required|email|unique:users,email|max:100',
+        'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        try {
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'role' => 'customer',
+            ]);
+
+            return redirect()
+                ->route('admin.dashboard')
+                ->with('success', 'Empleado creado correctamente.');
+        } catch (\Exception $e) {
+            return back()
+                ->with('error', 'No se pudo crear el empleado. Revisa los datos.')
+                ->withInput();
+        }
     }
 
 }
